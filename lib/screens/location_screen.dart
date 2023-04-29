e10728d1616b93835491a3754b2b6707db427aee
Washik Uddin Ahmed Mollah <washikuddinahmedmollah@Washiks-MacBook-Air.local>
2023-04-29 22:23:39 +0900

    second push

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -34,7 +34,7 @@ class _LocationScreenState extends State<LocationScreen> {
         cityName = "";
         return;
       }
-      double temp = weatherData["main"]["temp"];
+      int temp = weatherData["main"]["temp"];
       temperature = temp.toInt();
       message = weatherModel.getMessage(temperature);
       var condition = weatherData['weather'][0]['id'];
